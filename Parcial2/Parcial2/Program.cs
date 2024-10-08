58fecb41f83b9b2b3ac5b88107f3fc6271094aad
Vanesa Acevedo Grajales <nesa6423@gmail.com>
2024-10-07 20:37:18 -0500

    Inicializacion codigo

---
+++ Parcial2/Parcial2/Program.cs
@@ -0,0 +1,63 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        // Parcial 2, (Adivina el número)
+
+        //Inicialización código
+
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("¡Bienvenido al juego 'Adivina el número'!");
+
+                int numeroDeJugadores;
+
+                do
+                {
+                    Console.Write("Introduce el número de jugadores (2-4): ");
+                } while (!int.TryParse(Console.ReadLine(), out numeroDeJugadores) || numeroDeJugadores < 2 || numeroDeJugadores > 4);
+
+                int min, max;
+                switch (numeroDeJugadores)
+                {
+                    case 2:
+                        min = 0; max = 50;
+                        break;
+
+                    case 3:
+                        min = 0; max = 100;
+                        break;
+
+                    case 4:
+                        min = 0; max = 200;
+                        break;
+                    default:
+                        Console.WriteLine($"Número de jugadores inválido");
+                        break;
+
+
+
+
+                }
+
+
+            }
+
+
+
+    }
+    }
+}
+
+
+
+
+            
+    
+
+
+
+
+
